ce254df1b464660a2e68eedd973f11d59208f90a
Stephen Akuffo <sa.akuffo@gmail.com>
2022-10-03 23:05:47 -0400

    terraform code push

---
+++ terraform/et_variables.tf
@@ -0,0 +1,19 @@
+variable "rg_name" {
+  default = "evangelistTechnologic-dell"
+}
+
+variable "et_region_primary" {
+  default = "eastus"
+}
+
+variable "work_environment" {
+  default = "Evangelists"
+}
+
+variable "team_name" {
+  default = "Core Evangelism Team"
+}
+
+variable "net_name" {
+  default = "etDellNet"
+}
---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
+resource "azurerm_resource_group" "etRG" {
+  name     = var.rg_name
+  location = var.et_region_primary
+
+  tags = {
+    Environment = var.work_environment
+    Team        = var.team_name
+  }
+}
+
+resource "azurerm_virtual_network" "etDNET" {
+  name                = var.net_name
+  address_space       = ["10.0.0.0/16"] # variable?
+  location            = var.et_region_primary
+  resource_group_name = azurerm_resource_group.etRG.name
+}
